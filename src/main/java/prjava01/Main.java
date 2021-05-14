4612fa793c599864f60f1861044cbd4fbb76627f
usuario <usuario@DESKTOP-4C2QV9O>
2021-05-15 00:19:13 +0200

    Versió 0.2 del projecte prjava01

--- src/main/java/prjava01/Main.java
+++ src/main/java/prjava01/Main.java
@@ -20,14 +20,16 @@ public class Main {
             bw.write("<html>");bw.newLine();
             bw.write("  <head>");bw.newLine();
             bw.write("    <title>");bw.newLine();
-		bw.write("      Nova p&agrave;gina  web");bw.newLine();
-		bw.write("    </title>");bw.newLine();
-		bw.write("  </head>");bw.newLine();
-		bw.write("  <body>");bw.newLine();
-		bw.write("    Nova p&agrave;gina web");bw.newLine();
-		bw.write("  </body>");bw.newLine();
-		bw.write("</html>");bw.newLine();
-		bw.close();
-		} }
+            bw.write("      Nova p&agrave;gina  web");bw.newLine();
+            bw.write("    </title>");bw.newLine();
+            bw.write("  </head>");bw.newLine();
+            bw.write("  <body>");bw.newLine();
+            bw.write(" <h1>DAW2 m08uf4eac4</h1>");bw.newLine(); 
+            bw.write("    Nova p&agrave;gina web");bw.newLine();
+            bw.write("  </body>");bw.newLine();
+            bw.write("</html>");bw.newLine();
+            bw.close();
+        } 
+    }
     
 }
